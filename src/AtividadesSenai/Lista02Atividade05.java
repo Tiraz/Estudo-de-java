a8d7802a193be45e76b07adb8b7e4d5ed401b35d
Tiraz <hugo.suterio@hotmail.com>
2025-04-26 01:21:00 -0300

    Estudo atividade 02_05

--- src/AtividadesSenai/Lista02Atividade05.java
+++ src/AtividadesSenai/Lista02Atividade05.java
@@ -1,7 +1,19 @@
 package AtividadesSenai;
 
+import java.util.Scanner;
+
 public class Lista02Atividade05 {
     public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        Double area, perimetro, raio, pi = 3.1416;
 
+        System.out.println("Digite o valor do raio do circulo: ");
+        raio = teclado.nextDouble();
+
+        area = pi * (raio * raio);
+        perimetro = (raio * 2) * pi;
+
+        System.out.println("Área: " + area);
+        System.out.println("Perimetro: " + perimetro);
     }
 }
